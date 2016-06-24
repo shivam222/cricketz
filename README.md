b72a404fa7bf92ce40301056b96f501f44146220
shivam bansal <shivamb61@gmail.com>
2016-06-24 23:17:16 +0530

    php file added

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # cricketz
 A site on angularjs.
 cricket.html is the main page that contains the ng-routing and provides the ng-view tag for other pages to show up. 
+Right now i am working only on the structure of the site and not the real content.
---
+++ log.php
@@ -0,0 +1,52 @@
+<?php
+$servername = "localhost:3307";
+$username = "root";
+$password = "";
+$db_name="users";
+
+if($connection=mysql_connect($servername,$username,$password))
+{
+if($database=mysql_select_db($db_name,$connection)){
+	
+if(isset($_POST['logmail'])&&isset($_POST['logpass']))
+{
+$mail=$_POST['logmail'];$pass=$_POST['logpass'];
+$pass_hash=md5($pass);
+if(!empty($mail)&&!empty($pass))
+{
+    $query="SELECT id FROM users WHERE mail='$mail' AND password='$pass_hash'";
+	if($run=mysql_query($query))
+	{
+		$query_row=mysql_num_rows($run);
+	
+	if($query_row==0)
+	{
+		echo 'wrong password or email.';
+	}
+	else{
+		$user_id=mysql_result($run,0,'id');
+		echo $user_id; 
+	}
+	
+}
+else{
+	echo 'Query problem';
+}
+}
+else{
+	echo 'Bastard supply full information.';
+}
+}
+		
+}
+	else{
+		echo 'connection failed';
+	}
+}
+else{
+		echo 'connection failed';
+	}
+
+
+
+?>
