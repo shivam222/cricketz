c528cff1fbf3453c3881233f847f32b67d5692d1
shivam bansal <shivamb61@gmail.com>
2016-07-07 19:43:12 +0530

    player selection app improved

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ app.controller('legend',function($scope,$http){
 
 app.controller('ranker',function($scope,$http){
 	
-	 $http.get('aus20.json')//http://localhost:553/artifice_2540/jsonplayers.json
+	 $http.get('aus20bowl.json')//http://localhost:553/artifice_2540/jsonplayers.json
 	 .success(function(response){
 		   $scope.array=response.records; 
 	 });
@@ -203,9 +203,9 @@ app.controller('global',function($scope,$http){
 	 }
 	 
 	 $scope.submit=function(){
-		 if($scope.formatSelected==3 &&$scope.styleSelected==1 &&$scope.teamSelected=='Australia')
+		 if($scope.formatSelected==3 &&$scope.styleSelected==2 &&$scope.teamSelected=='Australia')
 		 {
-			 $http.get('aus20.json')
+			 $http.get('aus20bowl.json')
 			 .success(function(response)
 			 {
 				 $scope.array=response.records;
